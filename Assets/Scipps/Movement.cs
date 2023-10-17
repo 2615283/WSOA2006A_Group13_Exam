f55ed00f3e665e88075879eda5082e107c198632
2615283 <132197652+2615283@users.noreply.github.com>
2023-10-17 22:46:49 +0200

    ANimation stuff

--- Assets/Scipps/Movement.cs
+++ Assets/Scipps/Movement.cs
@@ -6,15 +6,39 @@ public class Movement : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rigbody;
-    private Vector2 inputMovement;
+    private Vector3 inputMovement;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
+        inputMovement = Vector3.zero;
+
         inputMovement.x = Input.GetAxisRaw("Horizontal");
         inputMovement.y = Input.GetAxisRaw("Vertical");
 
-        inputMovement.Normalize();
+        
 
         rigbody.velocity = inputMovement * speed;
+
+        UpdateAnims();
+    }
+
+    private void UpdateAnims()
+    {
+        if (inputMovement != Vector3.zero)
+        {
+            anim.SetFloat("Xmove", inputMovement.x);
+            anim.SetFloat("Ymove", inputMovement.y);
+            anim.SetBool("move", true);
+        }
+        else
+        {
+            anim.SetBool("move", false);
+        }
     }
 }
