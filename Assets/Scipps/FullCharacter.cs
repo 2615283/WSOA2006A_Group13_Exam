5cfe766d03d69fb969a0ddb5cb572266ff657965
2615283 <132197652+2615283@users.noreply.github.com>
2023-10-17 20:28:16 +0200

    Stuffies

--- Assets/Scipps/FullCharacter.cs
+++ Assets/Scipps/FullCharacter.cs
@@ -5,18 +5,12 @@ using UnityEngine;
 [CreateAssetMenu]
 public class FullCharacter : ScriptableObject
 {
-    public List<CharacterParts> parts = new List<CharacterParts>();
+    public CharacterParts[] parts;
 
     [System.Serializable]
     public class CharacterParts
     {
         public string partName;
-        public ScriptableObject part;
-
-        public CharacterParts(string _name, ScriptableObject _part)
-        {
-            partName = _name;
-            part = _part;
-        }
+        public CharacterCreatorData part;
     }
 }
---
+++ Assets/Scipps/PartsSelector.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartsSelector : MonoBehaviour
+{
+    [SerializeField]
+    private FullCharacter character;
+    [SerializeField] private PartSelection[] partSelections;
+   
+
+    private void Start()
+    {
+        for (int i = 0; i < partSelections.Length; i++)
+        {
+            CurrentParts(i);
+        }
+    }
+
+    public void NextPart(int IndexParts)
+    {
+        if (ConfirmValue(IndexParts))
+        {
+           if (partSelections[IndexParts].partCurrentInd < partSelections[IndexParts].partOptions.Length - 1)
+           {
+                partSelections[IndexParts].partCurrentInd++;
+           }
+           else
+           {
+                partSelections[IndexParts].partCurrentInd = 0;
+           }
+
+            UpdateParts(IndexParts);
+        }
+    }
+
+    public void PreviousPart(int IndexParts)
+    {
+        if (ConfirmValue(IndexParts))
+        {
+            if (partSelections[IndexParts].partCurrentInd > 0)
+            {
+                partSelections[IndexParts].partCurrentInd--;
+            }
+            else
+            {
+                partSelections[IndexParts].partCurrentInd = partSelections[IndexParts].partOptions.Length - 1;
+            }
+
+            UpdateParts(IndexParts);
+        }
+    }
+
+    private bool ConfirmValue(int IndexParts)
+    {
+        if (IndexParts > partSelections.Length || IndexParts < 0)
+        {
+            Debug.Log("Nope...");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void CurrentParts(int IndexParts)
+    {
+        //partSelections[IndexParts].partNameTXT.text = character.parts[IndexParts].part.partName;
+
+        partSelections[IndexParts].partCurrentInd = character.parts[IndexParts].part.partID;
+    }
+
+    private void UpdateParts(int IndexParts)
+    {
+        partSelections[IndexParts].partNameTXT.text = partSelections[IndexParts].partOptions[partSelections[IndexParts].partCurrentInd].partName;
+
+        character.parts[IndexParts].part = partSelections[IndexParts].partOptions[partSelections[IndexParts].partCurrentInd];
+    }
+
+    [System.Serializable]
+    public class PartSelection
+    {
+        public string partName;
+        public CharacterCreatorData[] partOptions;
+        public Text partNameTXT;
+        [HideInInspector] public int partCurrentInd;
+    }
+}
