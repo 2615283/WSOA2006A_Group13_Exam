49d687bd555c390761a0dc6b880b399ab5975cf6
2615283 <132197652+2615283@users.noreply.github.com>
2023-10-20 12:37:47 +0200

    Revert "Merge branch 'Art-and-Player-Character' of https://github.com/2615283/WSOA2006A_Group13_Exam into Art-and-Player-Character"
    This reverts commit 682a4410f7d6224f049b76917f647f5fe641d43e, reversing
    changes made to 68d952dccdd4559af4f03fc5a61989ef07941e0d.

--- Assets/Scipps/ShirtAnims.cs
+++ Assets/Scipps/ShirtAnims.cs
@@ -34,10 +34,10 @@ public class ShirtAnims : MonoBehaviour
 
     private void IdleShirtsBackOptions()
     {
-        if (spriteRenderer.sprite.name.Contains("Shirt_0_Idle_Back"))
+        if (spriteRenderer.sprite.name.Contains("Black_Shirt_Long_C_Back"))
         {
             string spriteName = spriteRenderer.sprite.name;
-            spriteName = spriteName.Replace("Shirt_0_Idle_Back_", "");
+            spriteName = spriteName.Replace("Black_Shirt_Long_C_Back_", "");
             int spriteNr = int.Parse(spriteName);
 
             spriteRenderer.sprite = shirtsBackIdle[shirtsNum].sprites[spriteNr];
@@ -46,10 +46,10 @@ public class ShirtAnims : MonoBehaviour
 
     private void IdleShirtsFrontOptions()
     {
-        if (spriteRenderer.sprite.name.Contains("Shirt_0_Idle_Forward"))
+        if (spriteRenderer.sprite.name.Contains("Black_Shirt_Long_C_Forward"))
         {
             string spriteName = spriteRenderer.sprite.name;
-            spriteName = spriteName.Replace("Shirt_0_Idle_Forward_", "");
+            spriteName = spriteName.Replace("Black_Shirt_Long_C_Forward_", "");
             int spriteNr = int.Parse(spriteName);
 
             spriteRenderer.sprite = shirtsFrontIdle[shirtsNum].sprites[spriteNr];
@@ -58,10 +58,10 @@ public class ShirtAnims : MonoBehaviour
 
     private void IdleShirtsLeftOptions()
     {
-        if (spriteRenderer.sprite.name.Contains("Shirt_0_Idle_Left"))
+        if (spriteRenderer.sprite.name.Contains("Black_Shirt_Long_C_Left"))
         {
             string spriteName = spriteRenderer.sprite.name;
-            spriteName = spriteName.Replace("Shirt_0_Idle_Left_", "");
+            spriteName = spriteName.Replace("Black_Shirt_Long_C_Left_", "");
             int spriteNr = int.Parse(spriteName);
 
             spriteRenderer.sprite = shirtsLeftIdle[shirtsNum].sprites[spriteNr];
@@ -70,10 +70,10 @@ public class ShirtAnims : MonoBehaviour
 
     private void IdleShirtsRightOptions()
     {
-        if (spriteRenderer.sprite.name.Contains("Shirt_0_Idle_Right"))
+        if (spriteRenderer.sprite.name.Contains("Black_Shirt_Long_C_Right"))
         {
             string spriteName = spriteRenderer.sprite.name;
-            spriteName = spriteName.Replace("Shirt_0_Idle_Right_", "");
+            spriteName = spriteName.Replace("Black_Shirt_Long_C_Right_", "");
             int spriteNr = int.Parse(spriteName);
 
             spriteRenderer.sprite = shirtsRightIdle[shirtsNum].sprites[spriteNr];
@@ -82,10 +82,10 @@ public class ShirtAnims : MonoBehaviour
 
     private void WalkShirtsBackOptions()
     {
-        if (spriteRenderer.sprite.name.Contains("Shirt_0_Walk_Back"))
+        if (spriteRenderer.sprite.name.Contains("Black_Shirt_Long_C_Back"))
         { 
             string spriteName = spriteRenderer.sprite.name;
-            spriteName = spriteName.Replace("Shirt_0_Walk_Back_", "");
+            spriteName = spriteName.Replace("Black_Shirt_Long_C_Back_", "");
             int spriteNr = int.Parse(spriteName);
 
             spriteRenderer.sprite = shirtsBackWalk[shirtsNum].sprites[spriteNr];
@@ -94,10 +94,10 @@ public class ShirtAnims : MonoBehaviour
 
     private void WalkShirtsFrontOptions()
     {
-        if (spriteRenderer.sprite.name.Contains("Shirt_0_Walk_Forward"))
+        if (spriteRenderer.sprite.name.Contains("Black_Shirt_Long_C_Forward"))
         {
             string spriteName = spriteRenderer.sprite.name;
-            spriteName = spriteName.Replace("Shirt_0_Walk_Forward_", "");
+            spriteName = spriteName.Replace("Black_Shirt_Long_C_Forward_", "");
             int spriteNr = int.Parse(spriteName);
 
             spriteRenderer.sprite = shirtsFrontWalk[shirtsNum].sprites[spriteNr];
@@ -106,10 +106,10 @@ public class ShirtAnims : MonoBehaviour
 
     private void WalkShirtsLeftOptions()
     {
-        if (spriteRenderer.sprite.name.Contains("Shirt_0_Walk_Left"))
+        if (spriteRenderer.sprite.name.Contains("Black_Shirt_Long_C_Left"))
         {
             string spriteName = spriteRenderer.sprite.name;
-            spriteName = spriteName.Replace("Shirt_0_Walk_Left_", "");
+            spriteName = spriteName.Replace("Black_Shirt_Long_C_Left_", "");
             int spriteNr = int.Parse(spriteName);
 
             spriteRenderer.sprite = shirtsLeftWalk[shirtsNum].sprites[spriteNr];
@@ -118,10 +118,10 @@ public class ShirtAnims : MonoBehaviour
 
     private void WalkShirtsRightOptions()
     {
-        if (spriteRenderer.sprite.name.Contains("Shirt_0_Walk_Right"))
+        if (spriteRenderer.sprite.name.Contains("Black_Shirt_Long_C_Right"))
         {
             string spriteName = spriteRenderer.sprite.name;
-            spriteName = spriteName.Replace("Shirt_0_Walk_Right_", "");
+            spriteName = spriteName.Replace("Black_Shirt_Long_C_Right_", "");
             int spriteNr = int.Parse(spriteName);
 
             spriteRenderer.sprite = shirtsRightWalk[shirtsNum].sprites[spriteNr];
